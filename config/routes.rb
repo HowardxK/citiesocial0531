e95a56c53415dcfe77628ec94072af301d5d4db6
howard <howard67890@gmail.com>
2020-05-31 13:17:59 +0800

    2.59

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,33 @@
 Rails.application.routes.draw do
-  root 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  root 'products#index'
+  
+  resources :products, only: [:index, :show]
+  resources :categories, only: [:show]   # /category/2
+  
+  resource :cart, only: [:show, :destroy] do
+    collection do
+      get :checkout
+    end
+  end
+
+  resources :orders, except: [:new, :edit, :update, :destroy]
+
+  namespace :admin do
+    root 'products#index'   # /admin
+    resources :products, except: [:show]
+    resources :vendors, except: [:show]
+    resources :categories, except: [:show] do
+      collection do
+        put :sort   # PUT /admin/categories/sort
+      end
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      post 'subscribe', to: 'utils#subscribe'
+      post 'cart', to: 'utils#cart'
+    end
+  end
 end
